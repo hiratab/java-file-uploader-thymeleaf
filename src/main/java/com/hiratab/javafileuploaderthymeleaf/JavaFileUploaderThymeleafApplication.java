2f9d7ffc24fd1050b3ef0769484a0e437b01e4e4
Bruno Hirata <brn.hirata@gmail.com>
2023-07-26 18:42:23 +0100

    Spring tutorial

--- src/main/java/com/hiratab/javafileuploaderthymeleaf/JavaFileUploaderThymeleafApplication.java
+++ src/main/java/com/hiratab/javafileuploaderthymeleaf/JavaFileUploaderThymeleafApplication.java
@@ -1,13 +1,27 @@
 package com.hiratab.javafileuploaderthymeleaf;
 
+import com.hiratab.javafileuploaderthymeleaf.configurations.StorageProperties;
+import com.hiratab.javafileuploaderthymeleaf.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class JavaFileUploaderThymeleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaFileUploaderThymeleafApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
---
+++ src/main/java/com/hiratab/javafileuploaderthymeleaf/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.hiratab.javafileuploaderthymeleaf.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
