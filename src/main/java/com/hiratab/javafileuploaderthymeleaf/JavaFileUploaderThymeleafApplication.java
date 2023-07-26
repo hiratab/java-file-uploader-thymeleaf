a263c3cc63c007b3f9ce62c009a53791cfb0df51
Bruno Hirata <brn.hirata@gmail.com>
2023-07-26 17:42:27 +0100

    Initial commit

---
+++ src/main/java/com/hiratab/javafileuploaderthymeleaf/JavaFileUploaderThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.hiratab.javafileuploaderthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFileUploaderThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFileUploaderThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiratab/javafileuploaderthymeleaf/JavaFileUploaderThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiratab.javafileuploaderthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFileUploaderThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
